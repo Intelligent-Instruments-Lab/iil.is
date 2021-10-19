03da9eeced68609dab3a4fac4345a6f014a1843b
Jack Armitage <jack.armitage@me.com>
2021-10-19 12:25:07 +0000

    Footer: added EU and ERC logotext

---
+++ src/components/Footer/ERC.svelte
@@ -0,0 +1,17 @@
+<script>
+  import eu_erc from '../../assets/svg/eu_erc.svg?url'
+</script>
+
+<div class="max-w-sm pr-8">
+  <img
+    class="
+      w-96
+      md:h-32 lg:h-36
+      object-cover object-center
+      mb-4"
+    src={eu_erc}
+    alt="European Research Council"/>
+  <p class="text-white text-xs">
+    The Intelligent Instruments project (INTENT) is funded by the European Research Council (ERC) under the European Union’s Horizon 2020 research and innovation programme (Grant agreement No. 101001848).
+  </p>
+</div>
---
+++ src/components/Footer/ERC_Mobile.svelte
@@ -0,0 +1,19 @@
+<script>
+  import eu_erc from '../../assets/svg/eu_erc.svg?url'
+</script>
+
+<div class="mt-8">
+  <img
+    class="
+      w-72
+      h-28
+      object-cover object-center
+      mb-8"
+    src={eu_erc}
+    alt="European Research Council"/>
+  <p class="text-white text-xs 
+    pr-4 max-w-sm
+    ">
+    The Intelligent Instruments project (INTENT) is funded by the European Research Council (ERC) under the European Union’s Horizon 2020 research and innovation programme (Grant agreement No. 101001848).
+  </p>
+</div>
