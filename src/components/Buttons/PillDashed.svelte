9f1670121a79fe11c42cb043352d34e8c3b9d483
Jack Armitage <jack.armitage@me.com>
2021-11-15 17:36:19 +0000

    Moving strings basic

--- src/components/Buttons/PillDashed.svelte
+++ src/components/Buttons/PillDashed.svelte
@@ -37,4 +37,39 @@
       {link.label}
     </a>
   </div>
+{:else if link.theme === 'dark_alt'}
+  <div class="
+    border-dashed border-white border-2
+    hover:border-primary-500
+    rounded-md">
+    <a
+      class="
+        pl-7 pr-7 pt-2 pb-2
+        bg-white hover:bg-primary-500
+        h-9 flex items-center justify-center 
+        text-primary-700 hover:text-white
+        font-hauser
+        uppercase"
+      href={link.url}
+      target={link.target}>
+      {link.label}
+    </a>
+  </div>
+{:else if link.theme === 'light_alt'}
+  <div class="
+    border-dashed border-white border-2
+    rounded-md">
+    <a
+      class="
+        pl-7 pr-7 pt-2 pb-2
+        h-9 flex items-center justify-center 
+        hover:bg-primary-500
+        text-white
+        font-hauser
+        uppercase"
+      href={link.url}
+      target={link.target}>
+      {link.label}
+    </a>
+  </div>
 {/if}
--- src/layouts/Events/MovingStrings.svelte
+++ src/layouts/Events/MovingStrings.svelte
@@ -1,6 +1,76 @@
 <script>
+  import { seo } from "../../stores/seo.js";
+  import { onMount } from 'svelte'
+  import { Layout } from '../../stores/layout.js'
+  import Menu from "../../components/Menu/Menu.svelte"
+  import CTARow from "../../components/Buttons/CTARow.svelte"
+
+  import title_graphic from '../../assets/svg/moving_strings.svg?url'
+
+  export let layout
+  export let date
+  export let title
+  export let description
+  export let featured
+  export let dates_fromto
+  export let copy
+  $seo.title = title
+  $seo.description = description
+  $seo.url = '/events/moving-strings'
+
+  let cta_links = [
+    { url: "#programme", label: "Programme", theme: "light_alt", target: "_self" },
+    { url: "#register", label: "Register", theme: "dark_alt", target: "_self" },
+  ]
+
+  onMount(async () => {
+    $Layout.menu = false
+    $Layout.page = layout
+    console.log('[Events - Moving Strings]', $Layout.page, layout)
+  })
   
 </script>
 
-<h1>Moving Strings</h1>
-
+{#if $Layout.menu}
+  <Menu/>
+{:else}
+  <!-- Content container -->
+  <div class="bg-primary-700">
+    <!-- Hero section -->
+    <div class="bg-primary-700
+      relative flex h-screen overflow-hidden"
+      >
+      <video autoplay loop muted
+        class="absolute z-10
+          w-auto min-w-full min-h-full max-w-none"
+        >
+        <source src="/vid/moving_strings.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div class="relative z-30 p-10">
+        <div class="mt-6 mb-12">
+          <img class="" src={title_graphic} alt="Moving Strings"/>
+        </div>
+        <div class="mb-8">
+          <!-- <p>{description}</p> -->
+          <p class="font-hauser uppercase text-white text-xl">
+            6-10 Dec, Reykjavík, Iceland
+          </p>
+        </div>
+        <div class="max-w-lg">
+          {#each copy as c}
+            <p class="text-white text-md">{c}</p>
+          {/each}
+        </div>
+        <!-- Buttons -->
+        <div class="mt-10 mb-10">
+          <CTARow links={cta_links}/>
+        </div>
+      </div>
+    </div>
+    <!-- Programme section -->
+    <div class="bg-gray-300 h-96 m-0"></div>
+    <!-- Other section -->
+    <div></div>
+  </div>
+{/if}
