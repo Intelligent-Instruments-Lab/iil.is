4e18e5316c5c191280aa71fe9e81f3ef07e3a5b5
Jack Armitage <jack.armitage@me.com>
2022-09-21 13:36:00 +0000

    fix

--- src/stores/seo.js
+++ src/stores/seo.js
@@ -4,5 +4,5 @@ export const seo = writable({
   title: "Intelligent Instruments Lab",
   description: "The Intelligent Instruments Lab designs instruments embedded with creative AI for musical performance. Our aim is to understand ourselves as users of intelligent technologies.",
   url: "/",
-  image: "/seo/sean_coils.jpeg",
+  image: "/seo/sean_coils.jpg",
 });
