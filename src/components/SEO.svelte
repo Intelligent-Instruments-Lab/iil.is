f32e122545cc1769e8d44817f857287365db10e2
Jack Armitage <jack.armitage@me.com>
2023-03-01 11:17:59 +0000

    seo test

--- src/components/SEO.svelte
+++ src/components/SEO.svelte
@@ -1,26 +1,29 @@
 <script>
-  import { seo } from "../stores/seo.js";
+  export let title
+  export let description
+  export let url
+  export let image
 </script>
 
 <svelte:head>
 
   <!-- Page -->
-  <title>{$seo.title} | Intelligent Instruments Lab</title>
-  <meta name="description" content={$seo.description}/>
+  <title>{title} | Intelligent Instruments Lab</title>
+  <meta name="description" content={description}/>
 
   <!-- Open Graph -->
-  <meta property="og:url" content={"https://iil.is" + $seo.url}>
-  <meta property="og:title" content={$seo.title + " | Intelligent Instruments Lab"}>
-  <meta property="og:description" content={$seo.description}>
-  <meta property="og:image" content={"https://iil.is" + $seo.image}>
+  <meta property="og:url" content={"https://iil.is" + url}>
+  <meta property="og:title" content={title + " | Intelligent Instruments Lab"}>
+  <meta property="og:description" content={description}>
+  <meta property="og:image" content={"https://iil.is" + image}>
   <meta property="og:type" content="website">
 
   <!-- Twitter -->
   <meta name="twitter:site" content="@_iil_is">
-  <meta property="twitter:url" content={"https://iil.is" + $seo.url}>
-  <meta property="twitter:title" content={$seo.title + " | Intelligent Instruments Lab"}>
-  <meta property="twitter:description" content={$seo.description}>
-  <meta property="twitter:image" content={"https://iil.is" + $seo.image}>
+  <meta property="twitter:url" content={"https://iil.is" + url}>
+  <meta property="twitter:title" content={title + " | Intelligent Instruments Lab"}>
+  <meta property="twitter:description" content={description}>
+  <meta property="twitter:image" content={"https://iil.is" + image}>
   <meta property="twitter:card" content="summary_large_image">
 
 </svelte:head>
