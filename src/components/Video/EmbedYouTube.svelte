7da11ff02d6f9f9e23886746ebaa5abd4e30f898
Jack Armitage <jack.armitage@me.com>
2023-11-27 17:13:21 +0000

    Add caption to EmbedYouTube component

--- src/components/Video/EmbedYouTube.svelte
+++ src/components/Video/EmbedYouTube.svelte
@@ -1,6 +1,7 @@
 <script>
   export let id
   export let scale
+  export let caption
 </script>
 
 <style>
@@ -19,13 +20,16 @@
   }
 </style>
 
-<div class="videoWrapper">
-  <iframe
-    width={scale ? scale * 560 : 560}
-    height={scale ? scale * 315 : 315}
-    src={"https://www.youtube.com/embed/"+id}
-    title="YouTube video player"
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-    allowfullscreen/>
+<div class="container">
+    <div class="videoWrapper">
+        <iframe
+        width={scale ? scale * 560 : 560}
+        height={scale ? scale * 315 : 315}
+        src={"https://www.youtube.com/embed/"+id}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen/>
+    </div>
+    <p class="text-primary-800 mt-2 text-sm">{caption}</p>
 </div>
